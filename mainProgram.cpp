9fd4db181d368bfc3c8787add26e035595c423de
Shine-Hexel <arsicandrakusuma@gmail.com>
2026-04-22 17:23:18 +0700

    project

--- mainProgram.cpp
+++ mainProgram.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
-
 int main() {
     cout << "Hello, World!" << endl;
+    cout << "This is a C++14 project." << endl;
+    cout << "Enjoy coding!" << endl;
     return 0;
 }
