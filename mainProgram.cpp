597f06e67e656d6998ee97569a52a932ae8f8141
PratitaAnnisa <rahmaannisapra@gmail.com>
2026-04-25 17:33:52 +0700

    list menu

--- mainProgram.cpp
+++ mainProgram.cpp
@@ -1,8 +1,55 @@
 #include <iostream>
 using namespace std;
-int main() {
-    cout << "Hello, World!" << endl;
-    cout << "This is a C++14 project." << endl;
-    cout << "Enjoy coding!" << endl;
-    return 0;
+void tampilMenu();
+
+int main(){
+	char pilih;
+	bool running = true;
+	
+	while (running){
+	tampilMenu();
+	cin >> pilih;
+	
+	    switch (toupper(pilih)) {
+			case 'A':
+				//addTask();
+				break;
+			case 'B':
+				//deleteTask();
+				break;
+			case 'C':
+				//updateTask();
+				break;
+			case 'D':
+				//searchTask();
+				break;
+			case 'E':
+				//sortTask();
+				break;
+			case 'F':
+				//doneTable();
+				break;
+			case 'G':
+				cout << "\n  Selamat fokus menyelesaikan tugas! Data tersimpan secara otomatis >_< \n\n";
+				running = false;
+				break;
+			default:
+				cout << "\n  [!] Pilihan tidak valid.\n";
+		}
+	}
+}
+
+void tampilMenu(){
+	cout << "========================================\n";
+    cout << "|              MENU UTAMA              |\n";
+    cout << "========================================\n";
+    cout << "    [A] Tambah Tugas Baru\n";
+    cout << "    [B] Hapus Tugas\n";
+    cout << "    [C] Update Status Tugas\n";
+    cout << "    [D] Cari Tugas\n";
+    cout << "    [E] Sorting Tugas\n";
+    cout << "    [F] Lihat Tugas Done\n";
+    cout << "    [G] Logout\n";
+    cout << "========================================\n";
+    cout << "  Pilihan: ";
 }
