aea88668ad1d34cac0cb1f87b0ef126816c8c416
Shine-Hexel <arsicandrakusuma@gmail.com>
2026-05-02 21:57:22 +0700

    a

--- mainProgram.cpp
+++ mainProgram.cpp
@@ -1,59 +1,77 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
-struct tugas{
-	string namaTugas;
-	string deskripsi;
-	string status;
-	int deadline;
-	tugas* next;
-	tugas* prev;
+struct tugas
+{
+    string namaTugas;
+    string deskripsi;
+    string status;
+    int deadline;
+    tugas *next;
+    tugas *prev;
 };
 
-tugas* head = nullptr;
-tugas* tail = nullptr;
+tugas *head = nullptr;
+tugas *tail = nullptr;
 
 void tampilMenu();
 
-int main(){
-	char pilih;
-	bool running = true;
-	
-	while (running){
-	tampilMenu();
-	cin >> pilih;
-	
-	    switch (toupper(pilih)) {
-			case 'A':
-				//addTask();
-				break;
-			case 'B':
-				//deleteTask();
-				break;
-			case 'C':
-				//updateTask();
-				break;
-			case 'D':
-				//searchTask();
-				break;
-			case 'E':
-				//sortTask();
-				break;
-			case 'F':
-				//doneTable();
-				break;
-			case 'G':
-				cout << "\n  Selamat fokus menyelesaikan tugas! Data tersimpan secara otomatis >_< \n\n";
-				running = false;
-				break;
-			default:
-				cout << "\nPilihan tidak valid!\nSilahkan pilih antara opsi A-G.\n";
-		}
-	}
+int main()
+{
+
+    int aa = 0;
+    while (aa < 10)
+    {
+        aa++;
+        if (aa == 10)
+            aa = 0;
+    }
+    // tampil task
+    char pilih;
+    bool running = true;
+
+    while (running)
+    {
+        tampilMenu();
+        cin >> pilih;
+
+        switch (toupper(pilih))
+        {
+        case 'A':
+            // addTask();
+            break;
+        case 'B':
+            // deleteTask();
+            break;
+        case 'C':
+            // updateTask();
+            break;
+        case 'D':
+            // searchTask();
+            break;
+        case 'E':
+            // sortTask();
+            break;
+        case 'F':
+            // doneTable();
+            break;
+        case 'H':
+            // tandai tugas selesai
+            break;
+        case 'G':
+            cout << "\n  Selamat fokus menyelesaikan tugas! Data tersimpan secara otomatis >_< \n\n";
+            running = false;
+            break;
+        default:
+            cout << "\nPilihan tidak valid!\nSilahkan pilih antara opsi A-G.\n";
+        }
+    }
 }
 
-void tampilMenu(){
-	cout << "========================================\n";
+void tampilMenu()
+{
+    cout << "========================================\n";
     cout << "|              MENU UTAMA              |\n";
     cout << "========================================\n";
     cout << "    [A] Tambah Tugas Baru\n";
@@ -62,6 +80,7 @@ void tampilMenu(){
     cout << "    [D] Cari Tugas\n";
     cout << "    [E] Sorting Tugas\n";
     cout << "    [F] Lihat Tugas Done\n";
+    cout << "    [H] Tandai Tugas Selesai\n";
     cout << "    [G] Logout\n";
     cout << "========================================\n";
     cout << "  Pilihan: ";
